5b5c157b6cc9e239536326c59e8aaa29b11b3de5
21seu <1693376841@qq.com>
2021-08-25 23:13:26 +0800

    集成Ant Design Vue

--- wiki-fe/src/main.js
+++ wiki-fe/src/main.js
@@ -1,4 +1,6 @@
 import Vue from 'vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 import './plugins/axios'
 import App from './App.vue'
 import router from './router'
@@ -6,6 +8,7 @@ import store from './store'
 import './plugins/element.js'
 
 Vue.config.productionTip = false
+Vue.use(Antd);
 
 new Vue({
   router,
