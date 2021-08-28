a4134c4fe4906a4428f15831ee59cfd994cd5092
21seu <1693376841@qq.com>
2021-08-28 18:43:53 +0800

    跨域问题

---
+++ src/main/java/com/ftj/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.ftj.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by fengtj on 2021/8/28 18:39
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)  //允许前端带凭证，一般用于校验登录sessionid，是否登陆过
+                .maxAge(3600);
+    }
+}
