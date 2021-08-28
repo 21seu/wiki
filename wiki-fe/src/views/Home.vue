74722c65f3afc097d3c601be6a12d70118a0d70a
21seu <1693376841@qq.com>
2021-08-28 20:12:23 +0800

    电子书列表界面

--- wiki-fe/src/views/Home.vue
+++ wiki-fe/src/views/Home.vue
@@ -46,23 +46,81 @@
         <a-layout-content
                 :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-            Content
+            <a-list item-layout="vertical" size="large" :data-source="ebooks"
+                    :grid="{ gutter: 20, column: 3 }">
+                <a-list-item slot="renderItem" key="item.title" slot-scope="item, index">
+                    <template v-for="{ type, text } in actions" slot="actions">
+        <span :key="type">
+          <a-icon :type="type" style="margin-right: 8px"/>
+          {{ text }}
+        </span>
+                    </template>
+                    <a-list-item-meta :description="item.description">
+                        <a slot="title" :href="item.href">{{ item.name }}</a>
+                        <a-avatar class="ant-avatar" slot="avatar" :src="item.cover"/>
+                    </a-list-item-meta>
+                </a-list-item>
+            </a-list>
         </a-layout-content>
     </a-layout>
 </template>
 
 <script>
-    import axios from 'axios';
     // @ is an alias to /src
+    import axios from 'axios';
+
+    const listData = [];
+    for (let i = 0; i < 23; i++) {
+        listData.push({
+            href: 'https://www.antdv.com/',
+            title: `ant design vue part ${i}`,
+            avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+            description:
+                'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+            content:
+                'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+        });
+    }
 
     export default {
         name: 'Home',
+        data() {
+            return {
+                ebooks: [],
+                listData,
+                pagination: {
+                    onChange: page => {
+                        console.log(page);
+                    },
+                    pageSize: 3,
+                },
+                actions: [
+                    {type: 'star-o', text: '156'},
+                    {type: 'like-o', text: '156'},
+                    {type: 'message', text: '2'},
+                ],
+            }
+        },
         created() {
             console.log("created");
+        },
+        mounted() {
+            let _this = this;
             axios.get("http://localhost:8080/ebook/list")
                 .then(response => {
-                    console.log(response)
-                })
+                    const data = response.data;
+                    _this.ebooks = data.content;
+                });
         }
     }
 </script>
+
+<style scoped>
+    .ant-avatar {
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 8%;
+        margin: 5px 0;
+    }
+</style>
