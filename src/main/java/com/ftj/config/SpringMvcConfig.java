6d88570e249650278d5bd8fc337563c7d179d8cd
21seu <1693376841@qq.com>
2021-08-31 00:09:40 +0800

    增加AOP，打印接口耗时，请求参数和返回参数 去掉过滤器和拦截器

--- src/main/java/com/ftj/config/SpringMvcConfig.java
+++ src/main/java/com/ftj/config/SpringMvcConfig.java
@@ -1,26 +1,26 @@
-package com.ftj.config;
-
-import com.ftj.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-/**
- * Created by fengtj on 2021/8/29 18:55
- */
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/login");
-    }
-}
+//package com.ftj.config;
+//
+//import com.ftj.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * Created by fengtj on 2021/8/29 18:55
+// */
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/login");
+//    }
+//}
