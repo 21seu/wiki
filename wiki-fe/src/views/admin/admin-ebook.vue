b80e614d49865b2ad9b3d6d5af5aa232d1a9c900
21seu <1693376841@qq.com>
2021-09-02 23:30:55 +0800

    电子书管理页面

--- wiki-fe/src/views/admin/admin-ebook.vue
+++ wiki-fe/src/views/admin/admin-ebook.vue
@@ -3,17 +3,60 @@
         <a-layout-content
                 :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-            <div class="about">
-                <h1>电子书管理</h1>
-            </div>
+            <a-table
+                    :columns="columns"
+                    :data-source="data"
+                    :row-key="record => record.id"
+                    :pagination="pagination"
+                    :loading="loading"
+                    @change="handleTableChange"
+            >
+                <template #cover="{ text: cover }">
+                    <img v-if="cover" :src="cover" alt="avatar"/>
+                </template>
+                <template v-slot:action="{ text: record }">
+                    <a-space size="small">
+                        <a-button type="primary">
+                            编辑
+                        </a-button>
+                        <a-button type="danger">
+                            删除
+                        </a-button>
+                    </a-space>
+                </template>
+            </a-table>
         </a-layout-content>
     </a-layout>
 </template>
 
 <script>
     export default {
-        name: "admin-ebook"
+        name: "AdminEbook"
     }
+    const columns = [
+        {
+            dataIndex: 'cover',
+            title: '封面',
+            slots: {customRender: 'cover'}
+        },
+        {
+            dataIndex: 'name',
+            title: '名称'
+        },
+        {
+            dataIndex: 'categoryId1',
+            title: '分类一',
+        },
+        {
+            dataIndex: 'categoryId2',
+            title: '分类二',
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            scopedSlots: {customRender: 'action'},
+        },
+    ];
 </script>
 
 <style scoped>
