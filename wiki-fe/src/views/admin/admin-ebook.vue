414bcd729a836893ae816cdb94d3dc58c1e2ab53
21seu <1693376841@qq.com>
2021-09-05 22:08:17 +0800

    电子书管理页面调整

--- wiki-fe/src/views/admin/admin-ebook.vue
+++ wiki-fe/src/views/admin/admin-ebook.vue
@@ -5,14 +5,14 @@
         >
             <a-table
                     :columns="columns"
-                    :data-source="data"
+                    :data-source="ebooks"
                     :row-key="record => record.id"
                     :pagination="pagination"
                     :loading="loading"
                     @change="handleTableChange"
             >
-                <template #cover="{ text: cover }">
-                    <img v-if="cover" :src="cover" alt="avatar"/>
+                <template slot="cover" slot-scope="text, record">
+                    <img class="ant-avatar" :src="record.cover" alt="avatar"/>
                 </template>
                 <template v-slot:action="{ text: record }">
                     <a-space size="small">
@@ -30,35 +30,87 @@
 </template>
 
 <script>
+    import axios from "axios";
+    import {message} from "ant-design-vue";
+
     export default {
-        name: "AdminEbook"
+        name: "AdminEbook",
+
+        data() {
+            return {
+                pagination: {
+                    current: 1,
+                    pageSize: 2,
+                    total: 0
+                },
+                ebooks: [],
+                loading: false,
+                columns: [
+                    {
+                        dataIndex: 'cover',
+                        title: '封面',
+                        scopedSlots: {customRender: 'cover'}
+                    },
+                    {
+                        dataIndex: 'name',
+                        title: '名称'
+                    },
+                    {
+                        dataIndex: 'categoryId1',
+                        title: '分类一',
+                    },
+                    {
+                        dataIndex: 'categoryId2',
+                        title: '分类二',
+                    },
+                    {
+                        title: 'Action',
+                        key: 'action',
+                        scopedSlots: {customRender: 'action'},
+                    },
+                ]
+            }
+        },
+
+        methods: {
+            handleQuery: function (params) {
+                //this.loading = true;
+                const _this = this;
+                axios.get("/ebook/list", params).then(resp => {
+                    console.log(resp.data.content);
+                    _this.ebooks = resp.data.content;
+
+                    //重置分页按钮
+                    if (params) _this.pagination.current = params.page;
+                });
+            },
+            handleTableChange: function (pagination) {
+                console.log("看看自带的分页参数：", pagination);
+                this.handleQuery({
+                    page: pagination.current,
+                    size: pagination.pageSize
+                });
+            }
+            ,
+        },
+
+        created() {
+            this.handleQuery();
+        }
+        ,
+
+        mounted() {
+
+        }
     }
-    const columns = [
-        {
-            dataIndex: 'cover',
-            title: '封面',
-            slots: {customRender: 'cover'}
-        },
-        {
-            dataIndex: 'name',
-            title: '名称'
-        },
-        {
-            dataIndex: 'categoryId1',
-            title: '分类一',
-        },
-        {
-            dataIndex: 'categoryId2',
-            title: '分类二',
-        },
-        {
-            title: 'Action',
-            key: 'action',
-            scopedSlots: {customRender: 'action'},
-        },
-    ];
 </script>
 
 <style scoped>
-
+    .ant-avatar {
+        width: 50px;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 8%;
+        margin: 5px 0;
+    }
 </style>
