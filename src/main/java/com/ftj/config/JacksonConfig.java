4fe5f1e73d31f143068c2d5ba58182c8f93c46ba
21seu <1693376841@qq.com>
2021-09-15 23:20:16 +0800

    雪花算法精度丢失问题解决，增加配置类

---
+++ src/main/java/com/ftj/config/JacksonConfig.java
@@ -0,0 +1,28 @@
+package com.ftj.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * Created by fengtj on 2021/9/15 23:15
+ */
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder){
+
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+
+        //全局配置序列化返回JSON处理
+        SimpleModule simpleModule = new SimpleModule();
+        //JSON Long ====> String
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        return objectMapper;
+    }
+}
